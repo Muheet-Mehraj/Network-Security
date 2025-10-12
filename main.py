218c5e90dd3ba2676c92337059a8e07349794c3c
Muheet Bhat <muheetmehraj93@gmail.com>
2025-10-12 21:07:45 +0530

    Data Transformation Done

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from Networksecurity.exception.exception import NetworkSecurityException
 from Networksecurity.logging.logger import logging
 from Networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig
 from Networksecurity.entity.config_entity import TrainingPipelineConfig
-
+from Networksecurity.components.data_transformation import DataTransformation
 from Networksecurity.entity.config_entity import ModelTrainerConfig
  
 
@@ -25,6 +25,15 @@ if __name__=='__main__':
         data_validation_artifact=data_validation.initiate_data_validation()
         logging.info("Data Validation Completed")
         print(data_validation_artifact)
+        data_transformation_config=DataTransformationConfig(trainingpipelineconfig)
+        logging.info("data Transformation started")
+        data_transformation=DataTransformation(data_validation_artifact,data_transformation_config)
+        data_transformation_artifact=data_transformation.initiate_data_transformation()
+        print(data_transformation_artifact)
+        logging.info("data Transformation completed")
+        
+
+        
         
         
         
